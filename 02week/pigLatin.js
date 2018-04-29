af38528c9321ae2b26a2230956e7afb95393aab7
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-04-29 11:51:45 -0500

    initial commit

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,20 +9,37 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
-  word = word.toLowerCase().trim();
-  const vowelIndex = word.search(/[aeiou]/)
-  const str1 = word.slice(vowelIndex)
-  const str2 = word.slice(0, vowelIndex);
-  let str3;
-  if (vowelIndex !== 0) {
-  str3 = 'ay'
-  } else {
-  str3 = 'yay'
+  word = word.toLowerCase().trim(); //trims word (or phrase) and lower cases lettes
+  if (/\s/.test(word)) { //checks if the word is actually a phrase (has spaces)
+    const wordArr = word.split(' '); //if so, splits the phrase into words
+    const pigLatinArr = [];
+    for (let i=0; i<wordArr.length; i++) {
+      if(/[a-z]/.test(wordArr[i])) {
+  pigLatinArr.push(pigLatin(wordArr[i])) //should I re-write my code to do this without recursion?
+}
+} //changes each word (words must include at least one letter) to pig latin and pushes the words into an array
+    return pigLatinArr.join(' ') //joins the array as a phrase
+
   }
-  const newStr = str1 + str2 + str3;
-  return newStr;
+  const vowels = ['a','e','i','o','u']; //list of vowels
+  const isVowel = (char) => vowels.indexOf(char) > -1; //checks if first character is a vowel
+  const firstToLast = (arr) => {
+    const first = arr.shift();
+    arr.push(first);
+    return arr;
+  } // moves the first element of an array to the last position
+  const charArr = word.split(''); // splits the word into an array of characters
+  let letterArr = charArr.filter(char => /[a-z]/.test(char)); //filters out all non-letter characters
+  if (isVowel(letterArr[0]))return letterArr.join('') + 'yay'; //if the first letter is a vowel returns the word + yay
+  for (let i=0; i< letterArr.length; i++) {
+    letterArr = firstToLast(letterArr);
+    if (isVowel(letterArr[0])) break;
+  } //keeps moving the first letter to the end of the letter array until the first letter is a vowel
+  return letterArr.join('') + 'ay'; //adds ay to the word if the first letter was not a vowel
 }
 
+/* I used regular expressions in this code, but only for my extra features of filtering out non-letter characters
+and translating phrases/sentences. */
 
 function getPrompt() {
   rl.question('word ', (answer) => {
