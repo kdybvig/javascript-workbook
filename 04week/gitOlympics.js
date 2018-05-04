1dbea7cf906bbab354b00aacfb17e6b3524ce59e
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-05-03 19:19:05 -0500

    added another comment

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -2,6 +2,6 @@
 
 const printListOfOlympians = (arr) => {
   arr.forEach(olympian => console.log(olympian))
-}
+} // console logs each olympian
 printListOfOlympians(['Joe','Bob','Tony','Alice','Kate'])
 //list of olympians
