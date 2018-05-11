c4704ac1fd47bfc2cd56772041391d244a986376
Keith Dybvig <kdybvig@keiths-mbp.bedichek.austinisd.org>
2018-05-11 12:52:49 -0500

    answered questions

--- 04week/loops.js
+++ 04week/loops.js
@@ -34,3 +34,38 @@ do {
   console.log(count);
   count++;
 } while (count < 1001);
+
+/*
+When is a for loop better than a while loop?
+Generally, a for loop is better than a while loop
+when the number of iterations is fixed, such as when
+iterating over the values in an array.  A while loop
+is more useful when a code will run over an unknown
+number of iterations until a stop condition is met.
+
+A for loop can also run code until a stop condition is met
+after an unknown number of iterations, but a while loop
+will be much more readable for this type of condition.
+
+Also, generally while loops tend to be a little easier
+to read, but with a while loop it is easier to accidentally
+create an infinite loop.
+*/
+
+/*
+What is the difference between a for loop and a for...in loop?
+A for...in loop iterates over all of the elements in an object, but
+in a non-sequential order.  A for...in loop can only be used for objects
+(including arrays), whereas a for loop can be used for many different
+purposes.  A for...in loop is a more convenient way to iterate over all
+the properties in an object than a for loop due to the fact that the
+properties in an object.
+*/
+
+/*
+What is the difference between a while loop and a do...while loop?
+A while loop only executes the code contained in the loop while the given
+condition is met.  A do while loop always executes the code contained in
+the loop once, and then afterwards continues to execute the code in the
+loop until the condition is met.
+*/
