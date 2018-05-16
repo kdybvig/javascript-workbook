eb3655ee1ab81d1bf08da982f006a7b6adb64bb2
Keith Dybvig <kdybvig@keiths-mbp.bedichek.austinisd.org>
2018-05-16 12:04:13 -0500

    inital commit, all tests passed

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -9,7 +9,35 @@ let jobTypes = {
   programmer: 'Any Ship!'
 };
 
-// Your code here
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+  }
+
+  enterShip(shipName) {
+    if(shipName) {
+      this.ship = shipName;
+      shipName.crew.push(this)
+    }
+  }
+}
+
+class Ship {
+  constructor (name, type, ability) {
+    this.name = name;
+    this.type = type;
+    this. ability = ability;
+    this.crew = [];
+  }
+
+  missionStatement() {
+    if(this.crew.length === 0) return "Can't perform a mission yet."
+      return this.ability
+  }
+
+}
 
 //tests
 if (typeof describe === 'function'){
